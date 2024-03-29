cf601d2ad974f67fe2ae75ec6c9fb57d6db53a94
Anthouse <65651730+l717177I@users.noreply.github.com>
2024-03-29 12:39:51 +0900

    [Silver III] Title: 근손실, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.c
+++ 백준/Silver/18429. 근손실/근손실.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #define WEIGHT 500
 // 백트래킹 - 순열을 활용한다.
-int n, k, cnt;
-int kit[10];
-int visited[10] = { 0 };
+int n, k, cnt = 0;
+int kit[10], visited[10];
 
 void inputData(void)
 {
@@ -11,32 +10,28 @@ void inputData(void)
 	for (int i = 0; i < n; i++) { scanf("%d", &kit[i]); }
 }
 
-// m일차,누적운동량
-void dfs(int m, int sum)
+void dfs(int m, int sum) // m일차,누적운동량
 {
-	if (sum < WEIGHT) return;
+	if (sum < WEIGHT) return; // 500 이하가 되는 케이스는 카운팅 x
 	if (m == n)	// n일차가 되었을 때
 	{
 		cnt += 1;
-		return;	// 500 이하가 되는 케이스는 카운팅 x
+		return;
 	}
 	sum -= k;
 	for (int i = 0; i < n; i++)
 	{
-		if (!visited[i])
-		{
-			visited[i] = 1;
-			dfs(m + 1, sum + kit[i]);
-			visited[i] = 0;
-		}
+		if (visited[i]) continue;
+		visited[i] = 1;
+		dfs(m + 1, sum + kit[i]);
+		visited[i] = 0;
 	}
 }
 
 int main(void)
 {
 	inputData();
-    cnt = 0;
-    dfs(0, 500);
+	dfs(0, 500);
 	printf("%d\n", cnt);
 	return 0;
 }
