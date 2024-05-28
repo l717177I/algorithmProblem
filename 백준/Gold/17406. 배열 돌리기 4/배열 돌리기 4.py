1a3e11255616102f288a4fd6df7b87cec9837bdf
Anthouse <65651730+l717177I@users.noreply.github.com>
2024-05-28 23:44:07 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 1064 ms, Memory: 31836 KB -BaekjoonHub

---
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+from copy import deepcopy
+
+N, M, K = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(N)]
+rcs = [list(map(int, input().split())) for _ in range(K)]
+
+ans = 987654321
+
+# 1. 회전 순서 정하기 (최대 6!=720)
+for p in permutations(rcs, K):
+    # 2. 회전
+    copy_a = deepcopy(a)  # 원본리스트 카피
+    for r, c, s in p:
+        r -= 1
+        c -= 1
+        for n in range(s, 0, -1):
+            tmp = copy_a[r-n][c+n]
+            copy_a[r-n][c-n+1:c+n+1] = copy_a[r-n][c-n:c+n]  # ->
+            for row in range(r-n, r+n):  # ↑
+                copy_a[row][c-n] = copy_a[row+1][c-n]
+            copy_a[r+n][c-n:c+n] = copy_a[r+n][c-n+1:c+n+1]  # <-
+            for row in range(r+n, r-n, -1):  # ↓
+                copy_a[row][c+n] = copy_a[row-1][c+n]
+            copy_a[r-n+1][c+n] = tmp
+
+    # 3. 각 행의 최소값 찾기
+    for row in copy_a:
+        ans = min(ans, sum(row))
+
+print(ans)
