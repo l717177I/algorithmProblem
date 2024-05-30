e140a882f92efb2baeb58a60a0e3efc24165f2a0
Anthouse <65651730+l717177I@users.noreply.github.com>
2024-05-30 18:31:10 +0900

    Create BOJ17406.c

---
+++ BOJ17406.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// 매 회 {9, 3, 1} 중 하나씩을 중복하지 않고 사용하였을 때, 가장 적은 횟수로 오링시키는 케이스를 찾아라
+// SCV {60, 60, 60} 일 때 최대 시간복잡도 3 * 3 * 60
+// https://velog.io/@qwerty1434/%EB%B0%B1%EC%A4%80-12869%EB%B2%88-%EB%AE%A4%ED%83%88%EB%A6%AC%EC%8A%A4%ED%81%AC
+int main(void)
+{
+    return 0;
+}
