38adf61a26b41d922ab4d8066e196dada88b9d1b
Anthouse <65651730+l717177I@users.noreply.github.com>
2024-06-09 19:59:03 +0900

    [Gold I] Title: 구슬 탈출, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.c
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 #define MAX 10
-#define RED 0
-#define BLUE 1
 int n, m;
 int sry, srx, sby, sbx;
 char board[MAX + 2][MAX + 2];
@@ -10,31 +8,25 @@ char board[MAX + 2][MAX + 2];
 int visited[MAX + 2][MAX + 2][MAX + 2][MAX + 2];
 int wp, rp;
 
-typedef struct {
+typedef struct
+{
 	int ry, rx, by, bx, cnt;
-} QUEUE;
+}QUEUE;
 
 QUEUE que[50000];
-
-void push(int ry, int rx, int by, int bx, int cnt)  // queue에 푸시
+void markingAndPush(int ry, int rx, int by, int bx, int cnt)
 {
+	visited[ry][rx][by][bx] = 1;
 	que[wp].ry = ry; que[wp].rx = rx;
 	que[wp].by = by; que[wp].bx = bx;
 	que[wp++].cnt = cnt;
 }
-
 QUEUE pop(void) { return que[rp++]; }   // queue popleft()
 
 int isWall(int y, int x)
 {
 	if (board[y][x] == '#') return 1;
 	return 0;
-}
-
-void checkVisited(int ry, int rx, int by, int bx, int cnt)
-{
-	visited[ry][rx][by][bx] = 1;
-	push(ry, rx, by, bx, cnt);
 }
 
 void inputData(void)
@@ -62,43 +54,49 @@ void inputData(void)
 int bfs(void)
 {
 	int sol = 0;
-	int dy[] = { 1, -1, 0, 0 };
-	int dx[] = { 0, 0, 1, -1 };
+	int dy[] = { 1,-1,0,0 };
+	int dx[] = { 0,0,1,-1 };
 	wp = rp = 0;
-	push(sry, srx, sby, sbx, 0);
-	visited[sry][srx][sby][sbx] = 1;
+	markingAndPush(sry, srx, sby, sbx, 0);
 	while (wp != rp)
 	{
 		QUEUE cur = pop();
-		if (cur.cnt > 10) break; // 10번 이상이면 종료
-		if (board[cur.ry][cur.rx] == 'O' && board[cur.by][cur.bx] != 'O')
+		if (cur.cnt > 10) break;	// 카운트 10회 이상이면 종료
+		if (board[cur.ry][cur.rx] == 'O') // 빨간구슬 구멍 도착
 		{
 			sol = 1;
 			break;
 		}
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 4; i++) // 굴리기
 		{
-			int nry = cur.ry, nrx = cur.rx, nby = cur.by, nbx = cur.bx;
-
-			while (!isWall(nry + dy[i], nrx + dx[i]) && board[nry][nrx] != 'O') { nry += dy[i]; nrx += dx[i]; }
-			while (!isWall(nby + dy[i], nbx + dx[i]) && board[nby][nbx] != 'O') { nby += dy[i]; nbx += dx[i]; }
-
-			if (board[nby][nbx] == 'O') continue; // 파란 구슬이 구멍에 빠지면 무시
-
-			if (nry == nby && nrx == nbx) // 겹치는 경우 처리
+			int nry = cur.ry;
+			int nrx = cur.rx;
+			int nby = cur.by;
+			int nbx = cur.bx;
+			while (!isWall(nry + dy[i], nrx + dx[i]) && board[nry][nrx] != 'O')
+			{
+				nry += dy[i];
+				nrx += dx[i];
+			}
+			while (!isWall(nby + dy[i], nbx + dx[i]) && board[nby][nbx] != 'O')
+			{
+				nby += dy[i];
+				nbx += dx[i];
+			}
+			if (board[nby][nbx] == 'O') continue;	// 구멍에 빠진다면
+			if (nry == nby && nrx == nbx) // 빨강이와 파랑이는 똑같은 위치면 안됨
 			{
 				if (board[nry][nrx] != 'O')
 				{
-					int red_dist = abs(nry - cur.ry) + abs(nrx - cur.rx);
-					int blue_dist = abs(nby - cur.by) + abs(nbx - cur.bx);
-					if (red_dist > blue_dist) { nry -= dy[i]; nrx -= dx[i]; }
+					int rd = abs(nry - cur.ry) + abs(nrx - cur.rx);
+					int bd = abs(nby - cur.by) + abs(nbx - cur.bx);
+					if (rd > bd) { nry -= dy[i]; nrx -= dx[i]; }
 					else { nby -= dy[i]; nbx -= dx[i]; }
 				}
 			}
-
 			if (!visited[nry][nrx][nby][nbx])
 			{
-				checkVisited(nry, nrx, nby, nbx, cur.cnt + 1);
+				markingAndPush(nry, nrx, nby, nbx, cur.cnt + 1);
 			}
 		}
 	}
